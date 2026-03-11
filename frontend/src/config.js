d399ee51aaefc8cd2b5b8b35fb50ec2bafaa663a
xan-d <145882987+xan-d@users.noreply.github.com>
2026-03-10 23:58:09 -0700

    final tweaks to deploy automation

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,8 @@
+// ************************************************************
+//   DON'T CHANGE ENVS. THESE AREN'T INCLUDED IN ANY PUSHES SO
+//    EACH RESPECTIVE .ENV (SERVER/LOCAL) WILL REMAIN CORRECT
+//    WITHOUT ANY MANUAL UPDATES OT THESE FILES.
+// ************************************************************
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5173'; // local development
 //   const API_URL = ''; // server production
 export { API_URL };
