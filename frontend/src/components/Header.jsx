357002f3311a0fb09fdd3a1255d4479ac379caf4
xan-d <145882987+xan-d@users.noreply.github.com>
2026-03-11 22:50:40 -0700

    last deploy.yml test

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -36,7 +36,7 @@ export default function Header() {
 
     return (
         <header className="header">
-            <h1 className="app-title">🌿Plant Partnerz</h1>
+            <h1 className="app-title">🌿Plant Partner</h1>
 
             <div className="menu-container">
                 <button
